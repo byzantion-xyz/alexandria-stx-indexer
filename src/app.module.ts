20f9e16cd08c5efea453e7899a668ce3c89f98ee
koninbtc <konin@byzantion.xyz>
2022-06-03 09:32:21 +0200

    Add Prisma Mongo service as near-stream db connector

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { PrismaService } from './prisma.service'
+import { ConfigModule } from '@nestjs/config';
 
-import { MongooseModule } from '@nestjs/mongoose';
 import { IndexersModule } from './indexers/indexers.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ['config.env', '.env'], isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        uri: config.get('NEAR_STREAMER_DATABASE_URL'),
-        useNewUrlParser: true
-      }),
-      connectionName: 'near-streamer',
-      inject: [ConfigService]
-    }),
     IndexersModule
   ],
   controllers: [
     AppController
   ],
   providers: [
-    AppService,
-    PrismaService
+    AppService
   ],
 })
 export class AppModule {}

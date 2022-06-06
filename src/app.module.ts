e81244684f8abd8d3f6ec773dccbed9050c663a4
Jacob Hoekert Hoekert <jshoekert@gmail.com>
2022-06-06 09:38:45 -0400

    fixed merge of byz-438 and main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { IndexersModule } from './indexers/indexers.module';
 import { ScrapersModule } from './scrapers/scrapers.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ['config.env', '.env'], isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        uri: config.get('NEAR_STREAMER_DATABASE_URL'),
-        useNewUrlParser: true
-      }),
-      connectionName: 'near-streamer',
-      inject: [ConfigService]
-    }),
     IndexersModule,
     ScrapersModule
   ],

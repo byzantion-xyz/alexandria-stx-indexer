f80a774a68648140c4e19e744ca2bc068695f270
Jacob Hoekert Hoekert <jshoekert@gmail.com>
2022-06-09 15:33:39 -0400

    added pinning

--- src/scrapers/scrapers.module.ts
+++ src/scrapers/scrapers.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { NearScraperController } from './near-scraper/near-scraper.controller';
 import { NearScraperService } from './near-scraper/near-scraper.service';
+import { IpfsHelperService } from './providers/ipfs-helper.service';
 
 @Module({
   controllers: [NearScraperController],
   providers: [
     NearScraperService,
-    PrismaService
+    PrismaService,
+    IpfsHelperService
   ]
 })
 export class ScrapersModule {}

9a59f096a69c04da450fa51915ab75663b8eb59d
Jacob Hoekert <jshoekert@gmail.com>
2022-06-16 13:22:54 -0400

    changed token from chain upper limit to 2000 for misfits

--- src/scrapers/near-scraper/near-scraper.service.ts
+++ src/scrapers/near-scraper/near-scraper.service.ts
@@ -506,7 +506,7 @@ export class NearScraperService {
       let tokenMetas = []
       let tokenMetaPromises = []
       if (startingTokenId < Number(collectionSize)) {
-        for (let i = startingTokenId; i < collectionSize; i++) {
+        for (let i = startingTokenId; i < 2000; i++) {
           const tokenMetaPromise = contract.nft_token({token_id: Number(i).toString()})
           tokenMetaPromises.push(tokenMetaPromise)
           if (i % 100 === 0) {

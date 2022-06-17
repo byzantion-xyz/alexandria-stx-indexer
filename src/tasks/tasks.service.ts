7bacc3e21f0b17934ce8cd97e4b28c68f69ce57e
koninbtc <konin@byzantion.xyz>
2022-06-17 09:21:42 +0200

    Set missings cron to run every 12 hours only in production

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -20,9 +20,9 @@ export class TasksService {
       }
     }
 
-    @Cron(CronExpression.EVERY_MINUTE)
+    @Cron(CronExpression.EVERY_12_HOURS)
     handleCronMissingTransactions() {
-      if (process.env.NODE_ENV !== 'production') {
+      if (process.env.NODE_ENV === 'production') {
         this.logger.log('Trigger near indexer for missing transactions');
         this.nearIndexer.runIndexerForMissing();
       } else {

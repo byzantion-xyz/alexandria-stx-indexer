b527c783f69123a8e3bb60f059d2c18fc6cddbd0
Jacob Hoekert <jshoekert@gmail.com>
2022-06-22 15:34:46 -0400

    fixed scraper controller to add token_series_id

--- src/scrapers/near-scraper/near-scraper.controller.ts
+++ src/scrapers/near-scraper/near-scraper.controller.ts
@@ -11,7 +11,7 @@ export class NearScraperController {
 
   @Post('scrape')
   async scrape(
-    @Body() data: { contract_key, token_id, starting_token_id, ending_token_id, scrape_non_custodial_from_paras, force_scrape}
+    @Body() data: { contract_key, token_series_id, token_id, starting_token_id, ending_token_id, scrape_non_custodial_from_paras, force_scrape}
   ) {
     const result = await this.nearScraper.scrape(data);
     return result;

0edf7dacd4ee926397e4f4f9637bfea4499d6c35
koninbtc <konin@byzantion.xyz>
2022-06-24 10:11:55 +0200

    Update migration to include index creation

--- prisma-streamer/migrations/20220623083412_initial/migration.sql
+++ prisma-streamer/migrations/20220623083412_initial/migration.sql
@@ -9,5 +9,10 @@
 ALTER TABLE "transaction" DROP COLUMN "receiver_id",
 DROP COLUMN "success_receipt_id";
 
+/* Add JSONB generated columns */
 ALTER TABLE public."transaction" ADD success_receipt_id text NULL GENERATED ALWAYS AS ((((outcome -> 'execution_outcome'::text) -> 'outcome'::text) -> 'status'::text) ->> 'SuccessReceiptId'::text) STORED;
 ALTER TABLE public."transaction" ADD receiver_id text NULL GENERATED ALWAYS AS (transaction ->> 'receiver_id'::text) STORED;
+
+/* Create indexes for jsonb generated colums */
+CREATE INDEX transaction_receiver_id_idx ON public.transaction USING btree (receiver_id);
+CREATE INDEX transaction_success_receipt_id_idx ON public.transaction USING btree (success_receipt_id);

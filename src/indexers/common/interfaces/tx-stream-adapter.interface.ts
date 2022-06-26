0e1d85f3521b6b7cd2015c3afadf2a60eb0ea5c5
koninbtc <konin@byzantion.xyz>
2022-06-26 16:52:10 +0200

    Drop unused dependency

--- src/indexers/common/interfaces/tx-stream-adapter.interface.ts
+++ src/indexers/common/interfaces/tx-stream-adapter.interface.ts
@@ -1,5 +1,4 @@
 import { TxProcessResult } from "src/indexers/common/interfaces/tx-process-result.interface";
-import { Transaction } from "src/indexers/near-indexer/dto/near-transaction.dto";
 import { CommonTx } from "./common-tx.interface";
 
 export interface TxStreamAdapter {

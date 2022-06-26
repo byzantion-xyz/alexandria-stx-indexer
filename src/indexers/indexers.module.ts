e153c60c25d8c4acbb9458e6dcfe6c453353e189
koninbtc <konin@byzantion.xyz>
2022-06-26 13:25:03 +0200

    Drop mongoose module

--- src/indexers/indexers.module.ts
+++ src/indexers/indexers.module.ts
@@ -4,7 +4,6 @@ import { NearIndexerService } from './near-indexer/near-indexer.service';
 import { BuyTransactionService } from './near-indexer/providers/buy-transaction.service';
 import { ListTransactionService } from './near-indexer/providers/list-transaction.service';
 import { TxHelperService } from './near-indexer/providers/tx-helper.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UnlistTransactionService } from './near-indexer/providers/unlist-transaction.service';
 import { DiscordBotModule } from 'src/discord-bot/discord-bot.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
@@ -13,7 +12,6 @@ import { NearTxStreamAdapterService } from './near-indexer/providers/near-tx-str
 
 @Module({
   imports: [
-    MongooseModule.forFeature([], 'near-streamer'),
     DiscordBotModule,
     PrismaModule,
     ScrapersModule

fd3ebb876249b4c60b2979460add25cfa086aab0
koninbtc <konin@byzantion.xyz>
2022-07-04 11:20:29 +0200

    Update near streamer query and transform only successful transactions

--- src/indexers/near-indexer/interfaces/near-transaction.dto.ts
+++ src/indexers/near-indexer/interfaces/near-transaction.dto.ts
@@ -2,17 +2,18 @@ import {
   TransactionsOutcome,
   TransactionsTransaction,
   ReceiptsReceipt,
-  ReceiptsExecutionOutcome
+  //ReceiptsExecutionOutcome
 } from '@internal/prisma/client';
+import { ExecutionOutcomeWithIdView } from 'near-api-js/lib/providers/provider';
 
 export interface Transaction {
   hash: string;
   outcome: TransactionsOutcome;
   transaction: TransactionsTransaction;
   receipt: ReceiptsReceipt;
-  execution_outcome: ReceiptsExecutionOutcome;
+  execution_outcome: ExecutionOutcomeWithIdView
   block_hash: string;
-  block_timestamp: bigint;
+  block_timestamp: bigint; 
   block_height: bigint;
   missing: boolean;
   processed: boolean;

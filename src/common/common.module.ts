2834d1a5aeafa1794bae71318b67078710c80fff
koninbtc <konin@byzantion.xyz>
2022-07-06 17:39:30 +0200

    Drop prisma from common module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,15 +1,13 @@
 import { Global, Module } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
 
-import { SmartContractService } from "./services/smart-contract/smart-contract.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SmartContract } from "src/entities/SmartContract";
 import { SmartContractFunction } from "src/entities/SmartContractFunction";
 
 @Global()
 @Module({
-  providers: [SmartContractService, PrismaService],
-  exports: [SmartContractService, PrismaService],
+  providers: [],
+  exports: [],
   imports: [TypeOrmModule.forFeature([SmartContract, SmartContractFunction])],
 })
 export class CommonModule {}

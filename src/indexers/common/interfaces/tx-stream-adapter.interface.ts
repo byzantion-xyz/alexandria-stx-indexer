2fad71f483182199dd20a164006a82369aabffa6
koninbtc <konin@byzantion.xyz>
2022-07-08 16:34:38 +0200

    Remove unused dependency

--- src/indexers/common/interfaces/tx-stream-adapter.interface.ts
+++ src/indexers/common/interfaces/tx-stream-adapter.interface.ts
@@ -1,7 +1,6 @@
 import { TxProcessResult } from "src/indexers/common/interfaces/tx-process-result.interface";
 import { CommonTx } from "./common-tx.interface";
 import { Client } from 'pg';
-import { IndexerEventType } from "../helpers/indexer-enums";
 
 export interface TxStreamAdapter {
   fetchTxs(): Promise<CommonTx[]>;

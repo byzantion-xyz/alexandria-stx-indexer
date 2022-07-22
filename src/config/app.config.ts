4ccf6175372e27b545bc9bc19adbd61dc3c71da7
koninbtc <konin@byzantion.xyz>
2022-07-22 12:36:09 +0200

    Drop chainSymbol from appConfig ( moved to indexerConfig)

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,5 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs('app', () => ({
-  configureDiscordServerApiKey: process.env.CONFIGURE_DISCORD_SERVER_API_KEY,
-  chainSymbol: process.env.CHAIN_SYMBOL || 'Near' 
+  configureDiscordServerApiKey: process.env.CONFIGURE_DISCORD_SERVER_API_KEY
 }));

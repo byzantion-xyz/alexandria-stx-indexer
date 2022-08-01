84eee3a5f500e879f25116912bced6643fc2b717
koninbtc <konin@byzantion.xyz>
2022-08-01 14:30:21 +0200

    Fix typo error

--- src/indexers/stacks-indexer/providers/bid-indexer.service.ts
+++ src/indexers/stacks-indexer/providers/bid-indexer.service.ts
@@ -28,7 +28,7 @@ export class BidIndexerService implements IndexerService {
     this.logger.debug(`process() ${tx.hash}`);
     let txResult: TxProcessResult = { processed: false, missing: false };
 
-    if (this.stacksTxHelper.isByzMarketplace(sc)) {
+    if (!this.stacksTxHelper.isByzMarketplace(sc)) {
       txResult.missing = true;
       return txResult;
     }

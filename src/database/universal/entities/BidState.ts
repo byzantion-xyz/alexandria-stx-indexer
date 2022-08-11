22b8ef85848e15508fadb484b99b49acb4c8a673
koninbtc <konin@byzantion.xyz>
2022-08-11 14:19:24 +0200

    Updates to order book bidding to use stacksTxHelper

--- src/database/universal/entities/BidState.ts
+++ src/database/universal/entities/BidState.ts
@@ -19,7 +19,7 @@ export class BidState {
   @Column("text")
   bid_buyer: string;
 
-  @Column("text")
+  @Column("text", { nullable: true })
   bid_seller: string;
 
   @Column("enum", { enum: ["active", "pending", "cancelled", "matched"], default: () => "active" })

523ad48eb0a23d4c4358c6c78fda2caf9a2f6fb4
koninbtc <konin@byzantion.xyz>
2022-08-29 13:31:14 +0200

    Improvements to handle custodial and non custodial

--- src/indexers/near-indexer/providers/accept-bid-indexer.service.ts
+++ src/indexers/near-indexer/providers/accept-bid-indexer.service.ts
@@ -20,24 +20,33 @@ export class AcceptBidIndexerService implements IndexerService {
     private txHelper: TxHelperService,
     private txBidHelper: TxBidHelperService,
     @InjectRepository(Action)
-    private actionRepository: Repository<Action>
+    private actionRepository: Repository<Action>,
+    @InjectRepository(SmartContract)
+    private smartContractRepository: Repository<SmartContract>
   ) {}
 
   async process(tx: CommonTx, sc: SmartContract, scf: SmartContractFunction): Promise<TxProcessResult> {
     this.logger.debug(`process() ${tx.hash}`);
     let txResult: TxProcessResult = { processed: false, missing: false };
-    let msc = Object.assign({}, sc);
+    let msc: SmartContract;
 
-    const contract_key = this.txHelper.extractArgumentData(tx.args, scf, 'contract_key');
     const token_id = this.txHelper.extractArgumentData(tx.args, scf, 'token_id'); 
     const price = this.txHelper.extractArgumentData(tx.args, scf, "price");
     const buyer = this.txHelper.extractArgumentData(tx.args, scf, 'buyer');
+    let contract_key = this.txHelper.extractArgumentData(tx.args, scf, 'contract_key');
+
+    if (sc.type.includes(SmartContractType.non_fungible_tokens)) {
+      msc = sc.custodial_smart_contract ? sc.custodial_smart_contract :
+        await this.smartContractRepository.findOneBy({ contract_key });
+      if (!msc) {
+        this.logger.log(`Marketplace smart_contract: ${contract_key} not found`);
+        txResult.missing = true;
+        return txResult;
+      }
+      contract_key = sc.contract_key;
+    }
 
     const nftMeta = await this.txHelper.findMetaByContractKey(contract_key, token_id);
-
-    if (sc.type.includes(SmartContractType.non_fungible_tokens) && sc.custodial_smart_contract) {
-      msc = sc.custodial_smart_contract;
-    }
 
     if (nftMeta) {
       await this.txHelper.unlistMetaInAllMarkets(nftMeta, tx, msc);

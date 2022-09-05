938b93d4464b3c7c1ff567961dcff3f1cdbaf263
koninbtc <konin@byzantion.xyz>
2022-09-05 12:43:40 +0200

    Fix BUG in stake indexer

--- src/indexers/near-indexer/providers/stake-indexer.service.ts
+++ src/indexers/near-indexer/providers/stake-indexer.service.ts
@@ -62,11 +62,10 @@ export class StakeIndexerService implements IndexerService {
       };
 
       if (this.txStakingHelper.isNewStakingBlock(tx, nftMeta.nft_state)) {
-        await this.txHelper.stakeMeta(nftMeta.id, tx, sc, stake_sc);
-        
-        await this.createAction(stakeActionParams);
-
         await this.txHelper.unlistMetaInAllMarkets(nftMeta, tx);
+
+        await this.txHelper.stakeMeta(nftMeta.id, tx, sc, stake_sc);
+        await this.createAction(stakeActionParams);       
       } else {
         this.logger.log(`Too Late`);
         await this.createAction(stakeActionParams);

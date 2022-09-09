e7ba2df72d8a2207fa589abf1b5a9e279d0d1f80
koninbtc <konin@byzantion.xyz>
2022-09-09 17:09:42 +0200

    Load stacks micro indexers into stacks indexers module

--- src/indexers/indexers.module.ts
+++ src/indexers/indexers.module.ts
@@ -2,48 +2,6 @@ import { Module } from "@nestjs/common";
 import { IndexerController } from "./indexer.controller";
 import { IndexerOrchestratorService } from "./indexer-orchestrator.service";
 import { ScrapersModule } from "src/scrapers/scrapers.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-
-import { StacksTxStreamAdapterService } from "./stacks-indexer/providers/stacks-tx-stream-adapter.service";
-
-import { StacksTxHelperService } from "./stacks-indexer/providers/stacks-tx-helper.service";
-import { TransferIndexerService as StacksTransferIndexerService } from './stacks-indexer/providers/transfer-indexer.service';
-
-import { ChangePriceIndexerService } from './stacks-indexer/providers/change-price-indexer.service';
-//import { BnsRegisterIndexerService } from './stacks-indexer/providers/bns-register-indexer.service';
-import { StacksMicroIndexersProvider } from "./common/providers/stacks-micro-indexers.service";
-//import { BnsBidIndexerService } from './stacks-indexer/providers/bns-bid-indexer.service';
-//import { BnsUnlistBidIndexerService } from './stacks-indexer/providers/bns-unlist-bid-indexer.service';
-//import { BnsAcceptBidIndexerService } from './stacks-indexer/providers/bns-accept-bid-indexer.service';
-import { BidIndexerService } from './stacks-indexer/providers/bid-indexer.service';
-import { UnlistBidIndexerService } from './stacks-indexer/providers/unlist-bid-indexer.service';
-import { AcceptBidIndexerService as StacksAcceptBidIndexerService } from './stacks-indexer/providers/accept-bid-indexer.service';
-import { CollectionOrderBookBidIndexerService } from './stacks-indexer/providers/collection-order-book-bid-indexer.service';
-import { CollectionOrderBookAcceptBidIndexerService } from './stacks-indexer/providers/collection-order-book-accept-bid-indexer.service';
-import { CollectionRemoveOrderBookBidIndexerService } from './stacks-indexer/providers/collection-remove-order-book-bid-indexer.service';
-import { CollectionMultiOrderBookBidIndexerService } from './stacks-indexer/providers/collection-multi-order-book-bid-indexer.service';
-import { IdBidIndexerService } from './stacks-indexer/providers/id-bid-indexer.service';
-import { MultiIdBidIndexerService } from './stacks-indexer/providers/multi-id-bid-indexer.service';
-import { IdAcceptBidIndexerService } from './stacks-indexer/providers/id-accept-bid-indexer.service';
-import { IdRemoveBidIndexerService } from './stacks-indexer/providers/id-remove-bid-indexer.service';
-import { SoloIdBidIndexerService } from './stacks-indexer/providers/solo-id-bid-indexer.service';
-import { SoloIdUnlistBidIndexerService } from './stacks-indexer/providers/solo-id-unlist-bid-indexer.service';
-import { SoloIdAcceptBidIndexerService } from './stacks-indexer/providers/solo-id-accept-bid-indexer.service';
-import { CollectionBidIndexerService } from './stacks-indexer/providers/collection-bid-indexer.service';
-import { CollectionUnlistBidIndexerService } from './stacks-indexer/providers/collection-unlist-bid-indexer.service';
-import { CollectionAcceptBidIndexerService } from './stacks-indexer/providers/collection-accept-bid-indexer.service';
-import { RelistIndexerService } from './stacks-indexer/providers/relist-indexer.service';
-import { RenameIndexerService } from './stacks-indexer/providers/rename-indexer.service';
-import { UpgradeIndexerService } from './stacks-indexer/providers/upgrade-indexer.service';
-import { TxUpgradeHelperService } from './stacks-indexer/helpers/tx-upgrade-helper.service';
-import { UpgradeMegaIndexerService } from './stacks-indexer/providers/upgrade-mega-indexer.service';
-import { ListIndexerService as StacksListIndexerService } from './stacks-indexer/providers/list-indexer.service';
-import { UnlistIndexerService as StacksUnlistIndexerService } from './stacks-indexer/providers/unlist-indexer.service';
-import { BuyIndexerService as StacksBuyIndexerService } from './stacks-indexer/providers/buy-indexer.service';
-import { StakeIndexerService as StacksStakeIndexerService } from './stacks-indexer/providers/stake-indexer.service';
-import { UnstakeIndexerService as StacksUnstakeIndexerService } from './stacks-indexer/providers/unstake-indexer.service';
-import { BuyWrapperIndexerService } from './stacks-indexer/providers/buy-wrapper-indexer.service';
-import { AdminSoloIdUnlistBidIndexerService } from './stacks-indexer/providers/admin-solo-id-unlist-bid-indexer.service';
 import { NearIndexerModule } from './near-indexer/near-indexer.module';
 import { CommonIndexerModule } from './common/common-indexer.module';
 import { StacksIndexerModule } from './stacks-indexer/stacks-indexer.module';
@@ -58,48 +16,7 @@ import { StacksIndexerModule } from './stacks-indexer/stacks-indexer.module';
   ],
   controllers: [IndexerController],
   providers: [
-    IndexerOrchestratorService,
-      
-    /* Micro indexers factory */
-   
-    StacksMicroIndexersProvider,
-  
-    /* Stacks micro indexers */
-    //BnsRegisterIndexerService,
-    //BnsBidIndexerService,
-    //BnsUnlistBidIndexerService,
-    //BnsAcceptBidIndexerService,
-    StacksListIndexerService,
-    StacksUnlistIndexerService,
-    StacksBuyIndexerService,
-    StacksStakeIndexerService,
-    StacksUnstakeIndexerService,
-    StacksTransferIndexerService,
-    ChangePriceIndexerService,
-    BidIndexerService,
-    UnlistBidIndexerService,
-    StacksAcceptBidIndexerService,
-    CollectionOrderBookBidIndexerService,
-    CollectionOrderBookAcceptBidIndexerService,
-    CollectionRemoveOrderBookBidIndexerService,
-    CollectionMultiOrderBookBidIndexerService,
-    IdBidIndexerService,
-    MultiIdBidIndexerService,
-    IdAcceptBidIndexerService,
-    IdRemoveBidIndexerService,
-    SoloIdBidIndexerService,
-    SoloIdUnlistBidIndexerService,
-    SoloIdAcceptBidIndexerService,
-    CollectionBidIndexerService,
-    CollectionUnlistBidIndexerService,
-    CollectionAcceptBidIndexerService,
-    RelistIndexerService,
-    RenameIndexerService,
-    UpgradeIndexerService,
-    TxUpgradeHelperService,
-    UpgradeMegaIndexerService,
-    BuyWrapperIndexerService,
-    AdminSoloIdUnlistBidIndexerService
+    IndexerOrchestratorService 
   ],
   exports: [IndexerOrchestratorService],
 })
--- src/indexers/stacks-indexer/stacks-indexer.module.ts
+++ src/indexers/stacks-indexer/stacks-indexer.module.ts
@@ -6,6 +6,39 @@ import { Block } from "src/database/stacks-stream/entities/Block";
 import { Transaction as StacksTransaction } from "src/database/stacks-stream/entities/Transaction";
 import { StacksTxStreamAdapterService } from './providers/stacks-tx-stream-adapter.service';
 import { StacksTxHelperService } from './providers/stacks-tx-helper.service';
+import { StacksMicroIndexersProvider } from '../common/providers/stacks-micro-indexers.service';
+import { ListIndexerService } from './providers/list-indexer.service';
+import { UnlistIndexerService } from './providers/unlist-indexer.service';
+import { BuyIndexerService } from './providers/buy-indexer.service';
+import { StakeIndexerService } from './providers/stake-indexer.service';
+import { UnstakeIndexerService } from './providers/unstake-indexer.service';
+import { TransferIndexerService } from './providers/transfer-indexer.service';
+import { ChangePriceIndexerService } from './providers/change-price-indexer.service';
+import { BidIndexerService } from './providers/bid-indexer.service';
+import { UnlistBidIndexerService } from './providers/unlist-bid-indexer.service';
+import { AcceptBidIndexerService } from './providers/accept-bid-indexer.service';
+import { CollectionOrderBookBidIndexerService } from './providers/collection-order-book-bid-indexer.service';
+import { CollectionOrderBookAcceptBidIndexerService } from './providers/collection-order-book-accept-bid-indexer.service';
+import { CollectionRemoveOrderBookBidIndexerService } from './providers/collection-remove-order-book-bid-indexer.service';
+import { CollectionMultiOrderBookBidIndexerService } from './providers/collection-multi-order-book-bid-indexer.service';
+import { IdBidIndexerService } from './providers/id-bid-indexer.service';
+import { MultiIdBidIndexerService } from './providers/multi-id-bid-indexer.service';
+import { IdAcceptBidIndexerService } from './providers/id-accept-bid-indexer.service';
+import { IdRemoveBidIndexerService } from './providers/id-remove-bid-indexer.service';
+import { SoloIdBidIndexerService } from './providers/solo-id-bid-indexer.service';
+import { SoloIdUnlistBidIndexerService } from './providers/solo-id-unlist-bid-indexer.service';
+import { SoloIdAcceptBidIndexerService } from './providers/solo-id-accept-bid-indexer.service';
+import { CollectionBidIndexerService } from './providers/collection-bid-indexer.service';
+import { CollectionUnlistBidIndexerService } from './providers/collection-unlist-bid-indexer.service';
+import { CollectionAcceptBidIndexerService } from './providers/collection-accept-bid-indexer.service';
+import { RelistIndexerService } from './providers/relist-indexer.service';
+import { RenameIndexerService } from './providers/rename-indexer.service';
+import { UpgradeIndexerService } from './providers/upgrade-indexer.service';
+import { TxUpgradeHelperService } from './helpers/tx-upgrade-helper.service';
+import { UpgradeMegaIndexerService } from './providers/upgrade-mega-indexer.service';
+import { BuyWrapperIndexerService } from './providers/buy-wrapper-indexer.service';
+import { AdminSoloIdUnlistBidIndexerService } from './providers/admin-solo-id-unlist-bid-indexer.service';
+//import { BnsRegisterIndexerService } from './providers/bns-register-indexer.service';
 
 @Module({
   imports: [
@@ -16,14 +49,50 @@ import { StacksTxHelperService } from './providers/stacks-tx-helper.service';
   providers: [
     StacksTxHelperService,
     { provide: 'TxStreamAdapter',  useClass: StacksTxStreamAdapterService },
-    //NearMicroIndexersProvider,
+    StacksMicroIndexersProvider,
     /* Stacks micro indexers */
+    ListIndexerService,
+    UnlistIndexerService,
+    BuyIndexerService,
+    StakeIndexerService,
+    UnstakeIndexerService,
+    TransferIndexerService,
+    ChangePriceIndexerService,
+    BidIndexerService,
+    UnlistBidIndexerService,
+    AcceptBidIndexerService,
+    CollectionOrderBookBidIndexerService,
+    CollectionOrderBookAcceptBidIndexerService,
+    CollectionRemoveOrderBookBidIndexerService,
+    CollectionMultiOrderBookBidIndexerService,
+    IdBidIndexerService,
+    MultiIdBidIndexerService,
+    IdAcceptBidIndexerService,
+    IdRemoveBidIndexerService,
+    SoloIdBidIndexerService,
+    SoloIdUnlistBidIndexerService,
+    SoloIdAcceptBidIndexerService,
+    CollectionBidIndexerService,
+    CollectionUnlistBidIndexerService,
+    CollectionAcceptBidIndexerService,
+    RelistIndexerService,
+    RenameIndexerService,
+    UpgradeIndexerService,
+    TxUpgradeHelperService,
+    UpgradeMegaIndexerService,
+    BuyWrapperIndexerService,
+    AdminSoloIdUnlistBidIndexerService
+    
+    //BnsRegisterIndexerService,
+    //BnsBidIndexerService,
+    //BnsUnlistBidIndexerService,
+    //BnsAcceptBidIndexerService,
   ],
   exports: [
     { provide: 'TxStreamAdapter', useClass: StacksTxStreamAdapterService },
     StacksTxHelperService,
     TypeOrmModule,
-    //NearMicroIndexersProvider
+    StacksMicroIndexersProvider
   ]
 })
 export class StacksIndexerModule {}

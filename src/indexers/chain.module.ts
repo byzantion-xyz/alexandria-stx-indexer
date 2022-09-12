1db6c1d447a8f890006435e10e7f62b4fffc6fc5
koninbtc <konin@byzantion.xyz>
2022-09-12 18:29:24 +0200

    Cleaning chain module

--- src/indexers/chain.module.ts
+++ src/indexers/chain.module.ts
@@ -7,20 +7,10 @@ import { NearMicroIndexersProvider } from './common/providers/near-micro-indexer
 import { NearTxHelperService } from './near-indexer/providers/near-tx-helper.service';
 import { NearTxStreamAdapterService } from './near-indexer/providers/near-tx-stream-adapter.service';
 
-import { BuyIndexerService } from './near-indexer/providers/buy-indexer.service';
-import { ListIndexerService } from './near-indexer/providers/list-indexer.service';
-import { UnlistIndexerService } from './near-indexer/providers/unlist-indexer.service';
-import { StakeIndexerService } from './near-indexer/providers/stake-indexer.service';
-import { UnstakeIndexerService } from './near-indexer/providers/unstake-indexer.service';
-import { AcceptBidIndexerService } from './near-indexer/providers/accept-bid-indexer.service';
-import { TransferIndexerService } from './near-indexer/providers/transfer-indexer.service';
-
-import { BurnIndexerService } from './near-indexer/providers/burn-indexer.service';
 import { StacksTxStreamAdapterService } from './stacks-indexer/providers/stacks-tx-stream-adapter.service';
 import { Block } from "src/database/stacks-stream/entities/Block";
 import { Transaction as StacksTransaction } from "src/database/stacks-stream/entities/Transaction";
 import { StacksMicroIndexersProvider } from './common/providers/stacks-micro-indexers.service';
-import { MissingCollectionService } from 'src/scrapers/near-scraper/providers/missing-collection.service';
 import { NearIndexerModule } from './near-indexer/near-indexer.module';
 import { StacksIndexerModule } from './stacks-indexer/stacks-indexer.module';
 import { TypeOrmModule } from '@nestjs/typeorm';

53230f6c7b0270b27ba32992cca0cf7cf172892b
koninbtc <konin@byzantion.xyz>
2022-09-14 00:57:04 +0200

    Drop discord tasks, config and modules (discord-bot and discord-server)

--- src/discord-server/interfaces/fetch-discord-server-channels.dto.ts
+++
@@ -1,6 +0,0 @@
-import { DiscordChannelType } from "src/indexers/common/helpers/indexer-enums";
-import { FetchUniversalChannels } from "./fetch-universal-channels.interface";
-
-export interface FetchDiscordServerChannels extends FetchUniversalChannels {
-  slug: string;
-}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DiscordBotModule } from 'src/discord-bot/discord-bot.module';
 import { IndexersModule } from 'src/indexers/indexers.module';
 import { TasksService } from './tasks.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    IndexersModule.register({ chainSymbol: process.env.CHAIN_SYMBOL }), 
-    DiscordBotModule
+    IndexersModule.register({ chainSymbol: process.env.CHAIN_SYMBOL })
   ],
   providers: [TasksService]
 })

f221f97618d0273b39f8f43752651efc1df50257
koninbtc <konin@byzantion.xyz>
2022-09-15 11:07:38 +0200

    Add nft_approve, nft_revoke and nft_transfer_call as generic Near functions

--- src/config/indexer.config.ts
+++ src/config/indexer.config.ts
@@ -21,30 +21,60 @@ export default registerAs("indexer", () => ({
       },
     ],
     Near: [
-    {
-      function_name: "nft_transfer",
-      name: "transfer",
-      args: {
-        "token_id": "token_id",
-        "buyer": "receiver_id"
+      {
+        function_name: "nft_transfer",
+        name: "transfer",
+        args: {
+          "token_id": "token_id",
+          "buyer": "receiver_id"
+        }
+      },
+      {
+        function_name: "nft_transfer_payout",
+        name: "transfer",
+        args: {
+          "token_id": "token_id",
+          "buyer": "receiver_id",
+          "price": "balance"
+        }
+      }, 
+      {
+        function_name: "nft_burn",
+        name: 'burn',
+        args: {
+          "token_id": "token_id"
+        }
+      }, 
+      {
+        function_name: 'nft_apporve',
+        name: 'list',
+        args: {
+          price: "msg.price",
+          token_id: "token_id",
+          list_action: "msg.market_type",
+          contract_key: "account_id",
+          token_price: "msg.sale_conditions.near",
+          buyer: "msg.buyer_id"
+        },
+      },
+      {
+        function_name: 'nft_revoke',
+        name: 'unlist',
+        args: { 
+          token_id: "token_id", 
+          contract_key: "account_id" 
+        },
+      },
+      {
+        function_name: 'nft_transfer_call',
+        name: 'stake',
+        args: { 
+          token_id: "token_id", 
+          contract_key: "receiver_id", 
+          action: "msg" 
+        }
       }
-    },
-    {
-      function_name: "nft_transfer_payout",
-      name: "transfer",
-      args: {
-        "token_id": "token_id",
-        "buyer": "receiver_id",
-        "price": "balance"
-      }
-    }, 
-    {
-      function_name: "nft_burn",
-      name: 'burn',
-      args: {
-        "token_id": "token_id"
-      }
-    }],
+    ]
   },
   blockRanges: {
     Stacks: {

d69ed94be91877c4ea82d154048a6e538030d065
agafox <agafox@byzantion.xyz>
2022-09-18 23:33:54 +0300

    Added: Implemented `defineFunctionCallIndexer()` logic

--- src/indexers/near-indexer/providers/near-tx-helper.service.ts
+++ src/indexers/near-indexer/providers/near-tx-helper.service.ts
@@ -2,8 +2,9 @@ import { Injectable, Logger } from '@nestjs/common';
 import { NftStateList } from 'src/database/universal/entities/NftStateList';
 import { CommonTx } from 'src/indexers/common/interfaces/common-tx.interface';
 import {IndexerTxEvent} from "../../../database/near-stream/entities/IndexerTxEvent";
-import {Receipt} from "../interfaces/near-indexer-tx-event.dto";
+import {NftOrFtEvent, Receipt} from "../interfaces/near-indexer-tx-event.dto";
 
+export const NEAR_EVENT_PREFIX = "EVENT_JSON:";
 
 @Injectable()
 export class NearTxHelperService {
@@ -57,4 +58,29 @@ export class NearTxHelperService {
         return decoded;
     }
   }
+
+  getEvents(rcpt: Receipt, acc: NftOrFtEvent[] = []) : NftOrFtEvent[] {
+      rcpt.logs.forEach((l) => {
+         if (l.startsWith(NEAR_EVENT_PREFIX)) {
+             const event: NftOrFtEvent = JSON.parse(l.replace(NEAR_EVENT_PREFIX, ''))
+             acc.push(event);
+         }
+      });
+
+      rcpt.receipts?.forEach((r) => {
+         this.getEvents(r, acc);
+      });
+
+      return acc;
+  }
+
+    getLogs(rcpt: Receipt, acc: string[] = []) : string[] {
+        rcpt.logs.forEach((l) => acc.push(l));
+
+        rcpt.receipts?.forEach((r) => {
+            this.getLogs(r, acc);
+        });
+
+        return acc;
+    }
 }

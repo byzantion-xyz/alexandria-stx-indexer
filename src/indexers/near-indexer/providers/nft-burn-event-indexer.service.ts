37857535ee294d53e53382ce391f390aba11b78f
koninbtc <konin@byzantion.xyz>
2022-09-19 16:32:45 +0200

    Add process to create burn actions

--- src/indexers/near-indexer/providers/nft-burn-event-indexer.service.ts
+++ src/indexers/near-indexer/providers/nft-burn-event-indexer.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, Logger } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { Action } from "src/database/universal/entities/Action";
 import { SmartContract } from "src/database/universal/entities/SmartContract";
 import { SmartContractFunction } from "src/database/universal/entities/SmartContractFunction";
 import { ActionName, SmartContractType } from "src/indexers/common/helpers/indexer-enums";
 import { TxHelperService } from "src/indexers/common/helpers/tx-helper.service";
 import { CommonTx } from "src/indexers/common/interfaces/common-tx.interface";
-import { CreateActionTO, CreateBurnActionTO } from "src/indexers/common/interfaces/create-action-common.dto";
+import { CreateBurnActionTO } from "src/indexers/common/interfaces/create-action-common.dto";
 import { IndexerService } from "src/indexers/common/interfaces/indexer-service.interface";
 import { TxProcessResult } from "src/indexers/common/interfaces/tx-process-result.interface";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class NftBurnEventIndexerService implements IndexerService {
@@ -15,6 +17,8 @@ export class NftBurnEventIndexerService implements IndexerService {
 
   constructor(
     private txHelper: TxHelperService,
+    @InjectRepository(Action)
+    private actionRepository: Repository<Action>,
   ) {}
 
   async process(tx: CommonTx, sc: SmartContract, scf: SmartContractFunction): Promise<TxProcessResult> {
@@ -67,7 +71,13 @@ export class NftBurnEventIndexerService implements IndexerService {
     return txResult;
   }
 
-  async createAction(params: CreateActionTO): Promise<Action> {
-    return;
+  async createAction(params: CreateBurnActionTO): Promise<Action> {
+    try {
+      const action = this.actionRepository.create(params);
+      const saved = await this.actionRepository.save(action);
+      this.logger.log(`New action ${params.action}: ${saved.id} `);
+
+      return saved;
+    } catch (err) { }
   }
 }

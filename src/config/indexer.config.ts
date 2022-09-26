3f5e4df0c77ae17e5a16a47cd98acab2db74391b
koninbtc <konin@byzantion.xyz>
2022-09-26 12:32:42 +0200

    Increase end block size for NEAR historical data

--- src/config/indexer.config.ts
+++ src/config/indexer.config.ts
@@ -89,7 +89,7 @@ export default registerAs("indexer", () => ({
     Near: {
       start_block_height: 42000000,
       start_block_height_tip: 42000000,
-      end_block_height: 74932000,
+      end_block_height: 74935500,
       block_range: 100000,
     },
   },

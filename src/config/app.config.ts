402621e82d51c62a1cd81f9f9cf5d7213ba5e1a4
koninbtc <konin@byzantion.xyz>
2022-09-27 11:03:55 +0200

    Update missings trigger for NEAR scraper to point alexandria-indexer-near-prod-2

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("app", () => ({
-  nearIndexerRunMissingUrl: "https://byz-universal-api-new.onrender.com/indexer/run-missing",
+  nearIndexerRunMissingUrl: "https://alexandria-indexer-near-prod-2.onrender.com/indexer/run-missing",
 }));

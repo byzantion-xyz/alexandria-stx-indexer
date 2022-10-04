c9b387c07506b94d89babcb91ce77ebdd11bd6ff
koninbtc <konin@byzantion.xyz>
2022-10-04 14:20:48 +0200

    Do not check for marketplace in unlist micro indexer as nft_revoke is not indexed anymore (replaced with delete_market_data)

--- src/indexers/near-indexer/providers/unlist-indexer.service.ts
+++ src/indexers/near-indexer/providers/unlist-indexer.service.ts
@@ -36,23 +36,6 @@ export class UnlistIndexerService implements IndexerService {
     const token_id = this.txHelper.extractArgumentData(tx.args, scf, "token_id");
     let contract_key = this.txHelper.extractArgumentData(tx.args, scf, "contract_key");
 
-    // Check if has custodial smart contract
-    if (sc.type.includes(SmartContractType.non_fungible_tokens)) {
-      const account_sc = await this.smartContractRepository.findOneBy({ contract_key });
-      if (account_sc && account_sc.type.includes(SmartContractType.marketplace)) {
-        msc = account_sc;
-      } else {
-        msc = sc.custodial_smart_contract ? sc.custodial_smart_contract : account_sc;
-      }
-
-      if (!msc) {
-        this.logger.log(`Marketplace smart_contract: ${contract_key} not found`);
-        txResult.missing = true;
-        return txResult;
-      }
-      contract_key = sc.contract_key;
-    }
-
     const nftMeta = await this.txHelper.findMetaByContractKey(contract_key, token_id);
     
     if (nftMeta) {

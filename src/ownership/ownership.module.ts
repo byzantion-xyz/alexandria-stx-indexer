decb326c27e65f2a60461a1c44d65df9fe087e28
koninbtc <konin@byzantion.xyz>
2022-10-07 17:16:12 +0200

    Compare differences between paras wallet nfts and universal wallet nfts

--- src/ownership/ownership.module.ts
+++ src/ownership/ownership.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NftMeta } from 'src/database/universal/entities/NftMeta';
 import { OwnershipController } from './ownership.controller';
 import { NearOwnershipService } from './providers/near-ownership.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([NftMeta])
+  ],
   providers: [NearOwnershipService],
   controllers: [OwnershipController],
   exports: [NearOwnershipService]
--- src/ownership/providers/near-ownership.service.ts
+++ src/ownership/providers/near-ownership.service.ts
@@ -2,14 +2,16 @@ import { Injectable, Logger } from '@nestjs/common';
 import * as fs from 'fs/promises';
 import * as path from 'path';
 import axios, { AxiosRequestConfig } from 'axios';
+import { wallets } from 'src/ownership/near-super-users';
+import { resourceLimits } from 'worker_threads';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NftMeta } from 'src/database/universal/entities/NftMeta';
+import { Repository } from 'typeorm';
+
 interface WalletNft {
   token_id: string
   contract_key: string
-  owner_id: string
-  token_series_id: string
 }
-import { wallets } from 'src/ownership/near-super-users';
-import { resourceLimits } from 'worker_threads';
 
 const PARAS_V2_API = 'https://api-v2-mainnet.paras.id';
 const BATCH_LIMIT = 50;
@@ -19,16 +21,24 @@ export class NearOwnershipService {
   private readonly logger = new Logger(NearOwnershipService.name);
 
   constructor(
-
+    @InjectRepository(NftMeta)
+    private nftMetaRepo: Repository<NftMeta>,
   ) {}
 
   async process (): Promise<void> {
     const wallets = await this.fetchSuperUsersWallets();
-    this.logger.log({ wallets });
 
     for (let wallet of wallets) {
       const walletNfts = await this.fetchWalletNfts(wallet);
+      const universalNfts = await this.fetchUniversalNfts(wallet);
 
+      // Symetrical diffrencen between both arrays.
+      const differences = this.compareResult(walletNfts, universalNfts);
+
+      // Drop missing from differences
+      if (differences.length) {
+        this.logger.log({ differences });
+      }
       break;
     }
 
@@ -42,11 +52,32 @@ export class NearOwnershipService {
     return owners
   }
 
+  async fetchUniversalNfts(wallet: string): Promise<WalletNft[]> {
+    const nftMetas = await this.nftMetaRepo.find({
+      where: {
+        nft_state: { owner: wallet }
+      },
+      relations: {
+        smart_contract: true
+      },
+      select: {
+        token_id: true,
+        smart_contract: { contract_key: true }
+      }
+    });
+
+    return nftMetas.map(i => ({
+      token_id: i.token_id,
+      contract_key: i.smart_contract.contract_key,
+    }));
+  }
+
   async fetchWalletNfts(wallet: string): Promise<WalletNft[]> {
     try {
       this.logger.log(`fetchWalletNfts() ${wallet}`)
       let results: WalletNft[] = [];
       let query: AxiosRequestConfig = {
+        timeout: 10000,
         params : {
           owner_id: wallet,
           __limit: 50,
@@ -65,17 +96,14 @@ export class NearOwnershipService {
        
         let result = data.data.results.map(r => ({
           token_id: r.token_id,
-          contract_key: r.contract_id,
-          owner_id: r.owner_id,
-          token_series_id: r.token_series_id
+          contract_key: r.contract_id
         }));
         results.push(...result);
         query.params.__skip += BATCH_LIMIT;
 
-        this.logger.log({ result });
-      } while (total === BATCH_LIMIT)
+      } while (total === BATCH_LIMIT);
 
-      return;
+      return results;
     } catch (err) {
       this.logger.warn(`fetchWalletNfts() failed for ${wallet}`);
       throw err;
@@ -88,7 +116,9 @@ export class NearOwnershipService {
   }
 
   compareResult(walletNfts: WalletNft[], universalNfts: WalletNft[]): WalletNft[] {
-    return;
+    return walletNfts.filter(a => 
+      !universalNfts.some(b=> a.contract_key == b.contract_key && a.token_id == b.token_id)
+    );
   }
 
   reportResult(walletNfts: WalletNft[]): void {

63158ec4b8095024c38696b29eb717491e266196
koninbtc <konin@byzantion.xyz>
2022-10-21 13:55:07 +0200

    Drop unnecessary logging

--- src/ownership/ownership.controller.ts
+++ src/ownership/ownership.controller.ts
@@ -29,7 +29,6 @@ export class OwnershipController {
   @Get('differences/near-active-wallets')
   async getNearActiveWalletsOwnership(): Promise<WalletNft[]> {
     const wallets: string[] = await this.nearOwnershipService.getActiveWallets('Near');
-    this.logger.log(wallets);
 
     const differences = await this.nearOwnershipService.process(wallets);
     return differences;

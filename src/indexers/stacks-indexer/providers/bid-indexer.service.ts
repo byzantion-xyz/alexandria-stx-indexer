46778e1eb26d2bb69ef9db44b8eef2a54307930d
koninbtc <konin@byzantion.xyz>
2022-10-21 10:28:00 +0200

    Update bid micro indexer to handle meta decoupling

--- src/indexers/stacks-indexer/providers/bid-indexer.service.ts
+++ src/indexers/stacks-indexer/providers/bid-indexer.service.ts
@@ -3,10 +3,14 @@ import { Action } from 'src/database/universal/entities/Action';
 import { SmartContract } from 'src/database/universal/entities/SmartContract';
 import { SmartContractFunction } from 'src/database/universal/entities/SmartContractFunction';
 import { ActionName, BidType } from 'src/indexers/common/helpers/indexer-enums';
-import { CreateBidCommonArgs, CreateBidStateArgs, TxBidHelperService } from 'src/indexers/common/helpers/tx-bid-helper.service';
+import { 
+  CreateBidCommonArgs, 
+  CreateBidStateArgs, 
+  TxBidHelperService 
+} from 'src/indexers/common/helpers/tx-bid-helper.service';
 import { TxHelperService } from 'src/indexers/common/helpers/tx-helper.service';
 import { CommonTx } from 'src/indexers/common/interfaces/common-tx.interface';
-import { CreateActionTO, CreateBidActionTO } from 'src/indexers/common/interfaces/create-action-common.dto';
+import { CreateBidActionTO } from 'src/indexers/common/interfaces/create-action-common.dto';
 import { IndexerService } from 'src/indexers/common/interfaces/indexer-service.interface';
 import { TxProcessResult } from 'src/indexers/common/interfaces/tx-process-result.interface';
 import { TxActionService } from 'src/indexers/common/providers/tx-action.service';
@@ -21,7 +25,7 @@ export class BidIndexerService implements IndexerService {
     private stacksTxHelper: StacksTxHelperService,
     private txBidHelper: TxBidHelperService,
     private txActionService: TxActionService
-  ) {}
+  ) { }
 
   async process(tx: CommonTx, sc: SmartContract, scf: SmartContractFunction): Promise<TxProcessResult> {
     let txResult: TxProcessResult = { processed: false, missing: false };
@@ -32,45 +36,40 @@ export class BidIndexerService implements IndexerService {
     }
 
     const contract_key = this.stacksTxHelper.extractAndParseContractKey(tx.args, scf);
-    const token_id = this.stacksTxHelper.extractArgumentData(tx.args, scf, 'token_id'); 
+    const token_id = this.stacksTxHelper.extractArgumentData(tx.args, scf, 'token_id');
     const price = this.stacksTxHelper.extractArgumentData(tx.args, scf, 'bid_price');
 
-    const nftMeta = await this.txHelper.findMetaByContractKey(contract_key, token_id);
+    const nftMeta = await this.txHelper.createOrFetchMetaByContractKey(contract_key, token_id, sc.chain_id);
 
-    if (nftMeta) {
-      const bidCommonArgs: CreateBidCommonArgs = this.txBidHelper.setCommonBidArgs(
-        tx, sc, nftMeta.collection, BidType.solo, price
-      );
-      const bidParams: CreateBidStateArgs = {
-        ... bidCommonArgs,
-        bid_buyer: tx.signer
-      };
-      let bidState = await this.txBidHelper.findActiveSoloBid(nftMeta, sc);
+    const bidCommonArgs: CreateBidCommonArgs = this.txBidHelper.setCommonBidArgs(
+      tx, sc, nftMeta.collection, BidType.solo, price
+    );
+    const bidParams: CreateBidStateArgs = {
+      ...bidCommonArgs,
+      bid_buyer: tx.signer
+    };
+    let bidState = await this.txBidHelper.findActiveSoloBid(nftMeta, sc);
 
-      if (this.txBidHelper.isNewBid(tx, bidState)) {
-        await this.txBidHelper.createOrReplaceBid(bidParams, bidState, nftMeta.id);
-      } else {
-        this.logger.log(`Too Late`);        
-      }
-
-      const actionCommonArgs = this.txHelper.setCommonActionParams(ActionName.bid, tx, nftMeta, sc);
-      const bidActionParams: CreateBidActionTO = {
-        ...actionCommonArgs,
-        bid_price: price,
-        buyer: tx.signer
-      };
-      await this.createAction(bidActionParams);
-
-      txResult.processed = true;
+    if (this.txBidHelper.isNewBid(tx, bidState)) {
+      await this.txBidHelper.createOrReplaceBid(bidParams, bidState, nftMeta.id);
     } else {
-      this.logger.debug(`NftMeta not found ${contract_key} ${token_id}`);
-      txResult.missing = true;
+      this.logger.debug(`Too Late`);
     }
+
+    const actionCommonArgs = this.txHelper.setCommonActionParams(ActionName.bid, tx, nftMeta, sc);
+    const bidActionParams: CreateBidActionTO = {
+      ...actionCommonArgs,
+      bid_price: price,
+      buyer: tx.signer
+    };
+    await this.createAction(bidActionParams);
+
+    txResult.processed = true;
 
     return txResult;
   }
 
-  async createAction(params: CreateActionTO): Promise<Action> {
+  async createAction(params: CreateBidActionTO): Promise<Action> {
     return await this.txActionService.saveAction(params);
   }
 

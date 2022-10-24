a8528fd91d844e96c8c0e2e5ccee77f997767e2f
koninbtc <konin@byzantion.xyz>
2022-10-24 11:41:44 +0200

    Update unlist micro indexer to handle meta decoupling

--- src/indexers/stacks-indexer/providers/unlist-indexer.service.ts
+++ src/indexers/stacks-indexer/providers/unlist-indexer.service.ts
@@ -21,7 +21,7 @@ export class UnlistIndexerService implements IndexerService {
     private txHelper: TxHelperService,
     private stacksTxHelper: StacksTxHelperService,
     private txActionService: TxActionService
-  ) {}
+  ) { }
 
   async process(tx: CommonTx, sc: SmartContract, scf: SmartContractFunction): Promise<TxProcessResult> {
     let txResult: TxProcessResult = { processed: false, missing: false };
@@ -29,32 +29,27 @@ export class UnlistIndexerService implements IndexerService {
     const token_id = this.stacksTxHelper.extractArgumentData(tx.args, scf, "token_id");
     const contract_key = this.stacksTxHelper.extractArgumentData(tx.args, scf, "contract_key");
 
-    const nftMeta = await this.txHelper.findMetaByContractKey(contract_key, token_id);
+    const nftMeta = await this.txHelper.createOrFetchMetaByContractKey(contract_key, token_id, sc.chain_id);
 
-    if (nftMeta) { 
-      const actionCommonArgs = this.txHelper.setCommonActionParams(ActionName[scf.name], tx, nftMeta, sc);
-      const nft_list_state = this.txHelper.findStateList(nftMeta.nft_state, sc.id);
+    const actionCommonArgs = this.txHelper.setCommonActionParams(ActionName[scf.name], tx, nftMeta, sc);
+    const nft_list_state = this.txHelper.findStateList(nftMeta.nft_state, sc.id);
 
-      const unlistActionParams: CreateUnlistActionTO = {
-        ...actionCommonArgs,
-        list_price: nft_list_state?.list_price,
-        seller: nft_list_state?.list_seller,
-        market_name: nft_list_state?.commission?.market_name || null,
-        commission_id: nft_list_state?.commission?.id
-      };
+    const unlistActionParams: CreateUnlistActionTO = {
+      ...actionCommonArgs,
+      list_price: nft_list_state?.list_price,
+      seller: nft_list_state?.list_seller,
+      market_name: nft_list_state?.commission?.market_name || null,
+      commission_id: nft_list_state?.commission?.id
+    };
 
-      if (this.stacksTxHelper.isNewerEvent(tx, nft_list_state)) {
-        await this.txHelper.unlistMeta(nftMeta, tx, sc);
-      } else {
-        this.logger.debug(`Too Late`);
-      }
-      await this.createAction(unlistActionParams);
-
-      txResult.processed = true;
+    if (this.stacksTxHelper.isNewerEvent(tx, nft_list_state)) {
+      await this.txHelper.unlistMeta(nftMeta, tx, sc);
     } else {
-      this.logger.debug(`NftMeta not found ${contract_key} ${token_id}`);
-      txResult.missing = true;
+      this.logger.debug(`Too Late`);
     }
+    await this.createAction(unlistActionParams);
+
+    txResult.processed = true;
 
     return txResult;
   }

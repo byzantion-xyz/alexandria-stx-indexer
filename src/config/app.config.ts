a38fd58343e752644535c58cdf1e4a65e7e9b790
Kelsonmudd <kelsonmudd@gmail.com>
2022-11-03 13:05:16 -0400

    Added set-suit-mapping microindexer

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,6 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("app", () => ({
   nearIndexerRunMissingUrl: "https://alexandria-indexer-near-prod-2.onrender.com/indexer/run-missing",
+  stacksScrapeUrl: "https://byz-stacks-scraper-prod.onrender.com/run-scrape",
   runFixNearOwnership:  process.env.RUN_FIX_NEAR_OWNERSHIP === 'true' || false,
 }));
--- src/indexers/stacks-indexer/providers/set-suit-mapping-indexer.service.ts
+++ src/indexers/stacks-indexer/providers/set-suit-mapping-indexer.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { Action } from 'src/database/universal/entities/Action';
 import { SmartContract } from 'src/database/universal/entities/SmartContract';
 import { SmartContractFunction } from 'src/database/universal/entities/SmartContractFunction';
@@ -7,6 +8,7 @@ import { CreateActionTO } from 'src/indexers/common/interfaces/create-action-com
 import { IndexerService } from 'src/indexers/common/interfaces/indexer-service.interface';
 import { TxProcessResult } from 'src/indexers/common/interfaces/tx-process-result.interface';
 import { StacksTxHelperService } from './stacks-tx-helper.service';
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class SetSuitMappingIndexerService implements IndexerService {
@@ -14,14 +16,25 @@ export class SetSuitMappingIndexerService implements IndexerService {
   stakingScs?: SmartContract[];
   
   constructor(
-    private stacksTxHelper: StacksTxHelperService
+    private stacksTxHelper: StacksTxHelperService,
+    private config: ConfigService,
   ) {}
 
   async process(tx: CommonTx, sc: SmartContract, scf: SmartContractFunction): Promise<TxProcessResult> {
     const txResult: TxProcessResult = { processed: false, missing: false };
     const token_id = this.stacksTxHelper.extractArgumentData(tx.args, scf, 'token_id');
     const contract_key = this.stacksTxHelper.extractArgumentData(tx.args, scf, 'contract_key');
+    const asset_name = this.stacksTxHelper.extractArgumentData(tx.args, scf, 'asset_name');
 
+    axios.post(
+      this.config.get("app.stacksScrapeUrl"),
+      {token_id, contract_key, asset_name},
+      {
+        headers: {
+        "x-api-key": process.env.BYZ_API_KEY,
+        }
+      }
+    );
 
     return txResult;
   }
